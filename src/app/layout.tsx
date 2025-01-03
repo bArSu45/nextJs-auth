2ead2bca7949281ccd34153fa1b8d68f80306613
bArSu45 <barsaraniojha@gmail.com>
2025-01-04 02:54:25 +0530

    REPO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import { Inter } from 'next/font/google';
 import { AuthProvider } from '@/components/AuthProvider';
 import './globals.css';
+import { ReactNode } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function RootLayout({ children }: any) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>
